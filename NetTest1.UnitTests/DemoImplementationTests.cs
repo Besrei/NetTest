70dca29fd04824b865bf8648cf3b13bfb2943dd3
Tomasz Wienchor <tomek.wienchor@gmail.com>
2022-05-26 01:30:21 +0200

    Added Tests for the first task

---
+++ NetTest1.UnitTests/DemoImplementationTests.cs
@@ -0,0 +1,85 @@
+﻿
+using FluentAssertions;
+using NetTest1;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NetTest1.UnitTests
+{
+    
+    public class DemoImplementationTests
+    {
+        private readonly DemoImplementation.Implementation _demoImplementation;
+        
+        public DemoImplementationTests()
+        {
+            _demoImplementation = new DemoImplementation.Implementation();
+        }
+
+        [Fact]
+        public void AddPersonWithEmail_ShouldCreatePersonWithEmail_WhenAllParametersAreValid()
+        {
+            //Arrange
+            string name = "Jamal";
+            string id = "123321";
+
+            string email = "jamal@jamal.com";
+            string emailType = "open";
+
+            var emailAddresses = new List<DemoSource.EmailAddress>()
+            {
+                new DemoSource.EmailAddress { Email = email, EmailType = emailType }
+            };
+
+            var person = new List<DemoSource.Person>()
+            {
+                new DemoSource.Person { Name = name, Id = id, Emails = emailAddresses }
+            } as IEnumerable<DemoSource.Person>;
+
+            var result = new List<DemoTarget.PersonWithEmail>()
+            {
+                new DemoTarget.PersonWithEmail {  FormattedEmail = $"{email}{emailType}", SanitizedNameWithId = $"{name}{id}" }
+            };
+
+            //Act
+            var methodResult = DemoImplementation.Implementation.Flatten(person).ToList();
+
+            //Assert
+            methodResult.Should().BeEquivalentTo(result);
+        }
+
+        [Theory]
+        [InlineData("", "", "", "")]
+        [InlineData("zdd##", "&&@", "email.com", "sdada")]
+        [InlineData("  ", "1233", "eeeme", "openo")]
+        [InlineData("Jamal", "123", "&(*A", "dodo")]
+        [InlineData("Jamal", "123", "jamal.jamal.jamal@compot.pl","doda")]
+        public void AddPersonWithEmail_ShouldNotCreatePersonWithEmail_WhenParametersAreInvalid(string name, string id, string email, string emailType)
+        {
+
+            //Arrange
+            var emailAddresses = new List<DemoSource.EmailAddress>()
+            {
+                new DemoSource.EmailAddress { Email = email, EmailType = emailType }
+            };
+
+            var person = new List<DemoSource.Person>()
+            {
+                new DemoSource.Person { Name = name, Id = id, Emails = emailAddresses }
+            } as IEnumerable<DemoSource.Person>;
+
+            var result = new List<DemoTarget.PersonWithEmail>()
+            {
+                new DemoTarget.PersonWithEmail {  FormattedEmail = $"{email}{emailType}", SanitizedNameWithId = $"{name}{id}" }
+            };
+
+            //Act
+            var methodResult = DemoImplementation.Implementation.Flatten(person).ToList();
+
+            //Assert
+            methodResult.Should().NotBeEquivalentTo(result);
+        }
+
+    }
+}
---
+++ NetTest1/DemoImplementation.cs
@@ -0,0 +1,49 @@
+﻿using DemoTarget;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DemoImplementation
+{
+    public class Implementation
+    {
+        /*Komentarz do zadania: Problemem w takim mapowaniu może być to, gdy każda z osób będzie miała ogromną ilość maili,
+         * Zostaniemy z tablelą pełną rekordów mająca wszystkie maile przypisane do pojedynczych imion.
+         */
+        public static IEnumerable<DemoTarget.PersonWithEmail> Flatten(IEnumerable<DemoSource.Person> people)
+        {
+            var result = new List<DemoTarget.PersonWithEmail>();
+
+            string personInputPattern = @"^[0-9a-zA-Z]+$";
+
+            string emailPattern = @"^[0-9a-zA-Z]+[.]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]{2,}[.][a-zA-Z]{2,3}$";
+
+            foreach (var person in people)
+            {
+                if (Regex.IsMatch(person.Name, personInputPattern) && Regex.IsMatch(person.Id, personInputPattern))
+                {
+                    AddPersonWithEmail(result, person, emailPattern);
+                }
+            }
+
+            return result;
+        }
+
+        private static void AddPersonWithEmail(List<PersonWithEmail> result, DemoSource.Person person, string emailPattern)
+        {
+            foreach (var email in person.Emails)
+            {
+                if (Regex.IsMatch(email.Email, emailPattern))
+                {
+                    result.Add(new DemoTarget.PersonWithEmail
+                    {
+                        //I'm not sure how the mail should be formatted
+                        FormattedEmail = $"{email.Email}{email.EmailType}",
+                        SanitizedNameWithId = $"{person.Name}{person.Id}"
+                    });
+                }
+            }
+            return;
+        }
+    }
+}
